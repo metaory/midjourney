8ba8cc8b954f9b9a0be8e00103ff8cb1517607d6
metaory <metaory@gmail.com>
2023-02-20 22:29:37 +0800

    updated readme

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ for file in $PWD/assets/*; do
   echo "${file##*/} in ./assets"
   echo "
 <p align="center">
+  <h5>${file##*/}</h5>
   <picture>
     <img width="90%" alt="mxflow" src="https://raw.githubusercontent.com/metaory/midjourney/master/assets/${file##*/}">
   </picture>
