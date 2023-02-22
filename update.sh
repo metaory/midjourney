b46106464dc8c8ddac2d2cd68cc6ac296fedcff4
metaory <metaory@gmail.com>
2023-02-22 21:00:33 +0800

    VVeiRj1pKMNcl9Am

--- update.sh
+++ update.sh
@@ -2,13 +2,15 @@
 
 echo "<h1>midjourney</h1>" > README.md
 
+i=0
 for file in $(ls -1t $PWD/assets | head -10); do
   echo "
 <p align="center">
-  <h5>${file}</h5>
+  <h5>${i}::${file}</h5>
   <picture>
     <img width="90%" alt="mxflow" src="https://raw.githubusercontent.com/metaory/midjourney/master/assets/${file}">
   </picture>
 </p>" >> README.md
+i=$((i + 1))
 done
 
