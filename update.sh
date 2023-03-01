ed9f4a3d5b6645d15cbb80551095926e85cac9ea
metaory <metaory@gmail.com>
2023-03-01 20:35:16 +0800

    PcJu+QZ6lcOwqwmf

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "<h1>midjourney</h1>" > README.md
+echo "<h1>Midjourney Wallpapers</h1>" > README.md
 
 echo "<h1>Total assets: [$(ls $PWD/assets | wc -l)]</h1>" > README.md
 
@@ -11,9 +11,6 @@ for file in $(ls -1t $PWD/assets | head -10); do
     <br>
     <h5>${i}::${file}</h5>
     <img width="90%" src="https://raw.githubusercontent.com/metaory/midjourney/master/assets/${file}" alt="${file}">
-    <br>
-    Midjourney Wallpapers
-    <br>
     <br>
   </h1>
   " >> README.md
